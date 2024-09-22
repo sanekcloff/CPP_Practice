4f12f2e7994e8a7413b0d339a5d3e9550ed541e1
sanekcl <sanekcl.sanek@gmail.com>
2024-09-22 21:01:42 +0300

    Update 22.09.2024

---
+++ Games/ExpensiveCalculator/ExpensiveCalculator.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "5 + 3 = " << 5 + 3  << endl;
+    cout << "5 - 3 = " << 5 - 3 << endl;
+    cout << "5 * 3 = " << 5 * 3 << endl;
+    cout << "5 / 3 = " << 5 / 3 << endl;
+    cout << "5.0 / 3.0 = " << 5.0 / 3.0 << endl;
+    cout << "5 % 3 = " << 5 % 3 << endl;
+    cout << "6 % 3 = " << 6 % 3 << endl;
+    cout << "5 * 3 + 2 = " << 5 * 3 + 2 << endl;
+    cout << "5 * (3 + 2) = " << 5 * (3 + 2) << endl;
+}
---
+++ Games/GameOver/GameOver.cpp
@@ -0,0 +1,16 @@
+﻿// GameOver
+// First C++ game!
+
+#include <iostream>
+
+using namespace std;
+
+//using std::cout;
+//using std::endl;
+
+int main()
+{
+    cout << "Game Over!\n";
+
+    return 0;
+}

c68401269adeb848014b4da02ec002c80ecbf67c
sanekcl <sanekcl.sanek@gmail.com>
2024-10-04 21:45:18 +0300

    04.10.2024

--- Games/GameStats/GameStats.cpp
+++ Games/GameStats/GameStats.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
-
+#include <string>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     int score;
     double distance;
     char playAgain;
@@ -25,12 +29,29 @@ int main()
     cout << "lives: " << lives << endl;
     cout << "aliensKilled: " << aliensKilled << endl;
     cout << "engineTemp: " << engineTemp << endl;
-    int fuel;
+
+    int fuel=0;
     cout << "How much fuel? ";
-    cin >> fuel;
+    string inputFuel;
+    cin >> inputFuel;
+    try
+    {
+        getline(cin, inputFuel);
+        fuel = stoi(inputFuel);
+    }
+    catch (const invalid_argument& e)
+    {
+        cerr << "Неверный аргумент: " << e.what() << endl;
+    }
+    catch (const out_of_range& e)
+    {
+        cerr << "Аргумент вне диапазона: " << e.what() << endl;
+    }
     cout << "fuel: " << fuel << endl;
+
     typedef unsigned short int ushort;
     ushort bonus = 10;
     cout << "\nbonus: " << bonus << endl;
+
     return 0;
 }

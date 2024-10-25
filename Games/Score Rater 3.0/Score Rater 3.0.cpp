d78cd79c6bca5e111332bdd48e727a308048e489
sanekcl <sanekcl.sanek@gmail.com>
2024-10-25 20:52:20 +0300

    Some update

---
+++ Games/Score Rater 3.0/Score Rater 3.0.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    typedef int MyCoolInt;
+    MyCoolInt score;
+    cout << "Enter yoour score\n";
+    cin >> score;
+    if (score >= 1000)
+    {
+        cout << "Your score 1000 or more!\n";
+    }
+    else if (score >= 500)
+    {
+        cout << "You score 500 or more!\n";
+    }
+    else if (score >= 250)
+    {
+        cout << "Yuo score 250 or more!\n";
+    }
+    else
+    {
+        cout << "Your score less than 250!\n";
+    }
+    return 0;
+}
+

7f6a8bb07b623dab4783677635a35f6327c33f86
sanekcl <sanekcl.sanek@gmail.com>
2024-10-28 21:14:47 +0300

    strings

---
+++ Games/Die Roller/Die Roller.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    typedef unsigned int uint;
+    uint castedTime = static_cast<uint>(time(0));
+    srand(castedTime);
+    // запускаем генератор случайных чисел
+    int randomNumber = rand();
+    int die = (randomNumber % 6) + 1; //Получаем число между 1 и 6
+    cout << "You rolled a " << die << endl;
+    cout << time(0);
+}
+
---
+++ Games/Guess My Number/Guess My Number.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    typedef unsigned int uint;
+    srand(static_cast<uint>(time(0)));
+    int secretNumber = rand() % 100 + 1;
+    int tries = 0;
+    int guess;
+    cout << "\tWelcome to Guess my Number\n\n";
+    do
+    {
+        cout << "Enter a guess: ";
+        cin >> guess;
+        ++tries;
+        if (guess > secretNumber)
+        {
+            cout << "To high!\n\n";
+        }
+        else if (guess < secretNumber)
+        {
+            cout << "To low!\n\n";
+        }
+        else
+        {
+            cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+        }
+    } while (guess != secretNumber);
+}
+
+    

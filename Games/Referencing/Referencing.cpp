e287abb8f91f11073f1c7100534c445c2c2988df
sanekcl <sanekcl.sanek@gmail.com>
2024-11-03 18:41:36 +0300

    Brain dead AI TIC TAC TOE

---
+++ Games/Referencing/Referencing.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myScore = 500;
+    int& mikesScore = myScore;
+    cout << "My score = " << myScore << ";\nMikes Score = " << mikesScore << endl;
+    myScore -= 100;
+    cout << "My score = " << myScore << ";\nMikes Score = " << mikesScore << endl;
+    mikesScore += 100;
+    cout << "My score = " << myScore << ";\nMikes Score = " << mikesScore << endl;
+}
---
+++ Games/Swap/Swap.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+void BadSwap(int a, int b);
+void GoodSwap(int& a, int& b);
+
+int main()
+{
+	int myScore = 500;
+	int yourScore = 1000;
+	cout << "\nMy score = " << myScore << "\nYour score = " << yourScore << endl;
+	BadSwap(myScore,yourScore);
+	cout << "\nMy score = " << myScore << "\nYour score = " << yourScore << endl;
+	GoodSwap(myScore,yourScore);
+	cout << "\nMy score = " << myScore << "\nYour score = " << yourScore << endl;
+
+}
+void BadSwap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void GoodSwap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
